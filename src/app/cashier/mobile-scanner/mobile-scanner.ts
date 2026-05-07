f337de4bfeeefa0894a8374782b36436c741d09a
VIKETA <crystintell@gmail.com>
2026-05-07 11:09:40 +0100

    done

--- src/app/cashier/mobile-scanner/mobile-scanner.ts
+++ src/app/cashier/mobile-scanner/mobile-scanner.ts
@@ -11,9 +11,11 @@ export class MobileScanner implements OnInit {
   ngOnInit(): void {
   }
     allowedFormats = [BarcodeFormat.QR_CODE, BarcodeFormat.EAN_13, BarcodeFormat.CODE_128];
-  
+  scanResults: string[] = [];
   onCodeResult(resultString: string) {
     console.log('Scanned Data:', resultString);
+    this.scanResults.push(resultString);
+
     // Add logic here to lookup product in POS
   }
 }
