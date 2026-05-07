73520de2fe78cf36fe20568899c550b9dd9097f5
VIKETA <crystintell@gmail.com>
2026-05-07 12:40:40 +0100

    m

--- src/app/cashier/mobile-scanner/mobile-scanner.ts
+++ src/app/cashier/mobile-scanner/mobile-scanner.ts
@@ -3,9 +3,10 @@ import { ZXingScannerModule } from '@zxing/ngx-scanner';
 import { BarcodeFormat } from '@zxing/library';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'mobile-scanner',
-  imports: [ZXingScannerModule,FormsModule,ReactiveFormsModule,CommonModule],
+  imports: [ZXingScannerModule,FormsModule,ReactiveFormsModule,CommonModule,ButtonModule],
   templateUrl: './mobile-scanner.html',
   styleUrl: './mobile-scanner.scss',
 })
@@ -15,10 +16,20 @@ export class MobileScanner implements OnInit {
   }
     allowedFormats = [BarcodeFormat.QR_CODE, BarcodeFormat.EAN_13, BarcodeFormat.CODE_128];
     currentDevice: MediaDeviceInfo | null = null;
+    availableDevices: MediaDeviceInfo[] = [];
   scannerEnabled = signal(false);
   transports: any[] = [];
   debugInfo: string = '';
   lastResult: string | null = null;
+
+
+  onCamerasFound(devices: MediaDeviceInfo[]): void {
+  this.availableDevices = devices;
+  if (devices.length > 0) {
+    // Default to the back camera (usually the last one in the array)
+    this.currentDevice = devices[devices.length - 1];
+  }
+}
 
   // // Success handler
   onCodeResult(resultString: string) {
