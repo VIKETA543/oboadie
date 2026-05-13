1c8448889dd74bbc68c0c73e9458cf75855a9668
VIKETA543 <crystintell@gmail.com>
2026-05-13 07:16:24 +0100

    Update app.routes.server.ts

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'home/auth/:uac',
+    renderMode: RenderMode.Server // This tells Angular NOT to prerender this at build time
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
