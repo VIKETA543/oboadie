c3ab4a5fb383a60409c1ce6a65dd7da48a56a70b
VIKETA543 <crystintell@gmail.com>
2026-05-14 16:21:29 +0000

    Update app.routes.server.ts

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,16 +1,12 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
- {
-  path: 'home/redirect-user/:uacp', // Add the comma here
-  renderMode: RenderMode.Server
-},
   {
-    path: 'admhome/store-manager/:uac',
+    path: 'home/redirect-user/:uacp',
     renderMode: RenderMode.Server
   },
   {
-    path: 'home/redirect-user/:uacp',
+    path: 'admhome/store-manager/:uac',
     renderMode: RenderMode.Server
   },
   {
