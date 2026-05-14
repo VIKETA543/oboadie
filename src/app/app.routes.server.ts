202a0cdf440aec3d01e1ec998c45408d044a74c8
VIKETA543 <crystintell@gmail.com>
2026-05-14 15:46:09 +0000

    Refactor server routes for consistency

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,13 +2,13 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: 'home/auth/:uac',
-    renderMode: RenderMode.Server // This tells Angular NOT to prerender this at build time
+    path: 'home/auth/:uac', renderMode: RenderMode.Server // This tells Angular NOT to prerender this at build time
   },
   {
-    path: 'admhome/store-manager/:uac',
-    renderMode: RenderMode.Server
-  }
+    path: 'admhome/store-manager/:uac',renderMode: RenderMode.Server
+  },{
+    'home/redirect-user/:uacp',renderMode: RenderMode.Server
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender
