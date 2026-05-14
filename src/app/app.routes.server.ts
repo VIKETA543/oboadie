18c6f7f7ed98052ad7dd8ec090148a0607046015
VIKETA543 <crystintell@gmail.com>
2026-05-14 16:38:17 +0000

    Update app.routes.server.ts

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,19 +2,19 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-  path: 'admhome/store-manager/:uac/store-receive-stock',
-  renderMode: RenderMode.Server // This fixes the error
+  path: 'admhome/store-manager/:uac',
+  renderMode: RenderMode.Server
 },
   {
-    path: 'admhome/store-manager/:uac/product-category',
-     renderMode: RenderMode.Server // This fixes the error
+    path: 'admhome/store-manager/:uac',
+     renderMode: RenderMode.Server
   },
 {
-  path:  'admhome/store-manager/:uac/store-type',
-      renderMode: RenderMode.Server // This fixes the error
+  path:  'admhome/store-manager/:uac',
+      renderMode: RenderMode.Server 
 },
 {path: 'home/auth/:uac',
-   renderMode: RenderMode.Server // This fixes the error
+   renderMode: RenderMode.Server
 } 
 
 ];
