3d0cebb1371cc17d69727b07ca3b64d9520c8a81
VIKETA543 <crystintell@gmail.com>
2026-05-14 16:01:28 +0000

    Update app.routes.server.ts

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -4,11 +4,12 @@ export const serverRoutes: ServerRoute[] = [
  {
   path: 'home/redirect-user/:uacp', // Add the comma here
   renderMode: RenderMode.Server
-}
+},
   {
     path: 'admhome/store-manager/:uac',
     renderMode: RenderMode.Server
-  },{
+  },
+  {
     'home/redirect-user/:uacp',
     renderMode: RenderMode.Server
   },
