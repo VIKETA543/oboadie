16467c8a16e232db83eb142c944290aaa1633641
VIKETA543 <crystintell@gmail.com>
2026-05-14 16:56:39 +0000

    Update app.routes.server.ts

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,15 +1,46 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  // --- DYNAMIC ROUTES (Must be Server rendered) ---
   {
-  path: 'admhome/store-manager/:uac',
-  renderMode: RenderMode.Server
-},
-{path: 'home/auth/:uac',
-   renderMode: RenderMode.Server
-} ,
-  {
-    path: 'redirect-user/:uacp',
+    path: 'home/auth/:uac',
     renderMode: RenderMode.Server
   },
+  {
+    path: 'home/redirect-user/:uacp',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'admhome/store-manager/:uac',
+    renderMode: RenderMode.Server
+  },
+  {
+    // Nested dynamic route
+    path: 'admhome/store-manager/:uac/:child', 
+    renderMode: RenderMode.Server
+  },
+
+  // --- STATIC ROUTES (Can be Prerendered) ---
+  {
+    path: 'home/sign-up',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'home/user-login',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'admhome/security-manager/uacgenerator',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'admhome/security-manager/department-hook',
+    renderMode: RenderMode.Prerender
+  },
+
+  // --- FALLBACK / WILDCARD ---
+  {
+    path: '**',
+    renderMode: RenderMode.Prerender
+  }
 ];
